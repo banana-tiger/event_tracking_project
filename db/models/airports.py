48c04d1a96fc047e152da945cb61f07e12b8b9d4
Anton Maliutin <amalutin13@gmail.com>
2020-03-23 20:28:31 +0300

    модели cities и airports

---
+++ db/models/airports.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Airports(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    code = db.Column(db.String, unique=True, nullable=False)
+    country_code = db.Column(db.String, unique=True, nullable=False)
+    flightable = db.Column(db.Bool, nullable=False)
+
+    def __repr__(self):
+        return '<Airports {} {} {}>'.format(self.country_code, self.name, self.code)
