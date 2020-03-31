f60b603c6fa9d7df390f3b5de2948396bbae5c4c
gitrus <gitrus@yandex.com>
2020-03-31 22:52:44 +0300

    Rearranged users to different blueprint. Fixed requirements.txt.

--- src/event_tracking_project/user/model.py
+++ src/event_tracking_project/user/model.py
@@ -1,14 +1,14 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_sqlalchemy import SQLAlchemy
+from ..db import db
 
-db = SQLAlchemy()
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.Text, nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
