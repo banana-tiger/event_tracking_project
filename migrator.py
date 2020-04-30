696c5f757aa436279ddcd24979628b546d8d81af
gitrus <gitrus@yandex.com>
2020-04-30 23:33:39 +0300

    Fix migration.

--- migrator.py
+++ migrator.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from flask import Flask
 
 from flask_migrate import Migrate
@@ -10,10 +12,13 @@ from src.event_tracking_project.user.model import Users
 
 from src.event_tracking_project.lastfm_crawler.model import Event
 
+THIS_DIR = Path(__file__).absolute().parent
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('src\event_tracking_project\config.py')
+    config_path = THIS_DIR / "src" / "event_tracking_project" / "config.py"
+    app.config.from_pyfile(config_path)
     db.init_app(app)
     migrate = Migrate(app, db, directory=PATH_TO_DB_MIGRATIONS)
 
